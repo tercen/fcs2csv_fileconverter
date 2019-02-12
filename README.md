574b3bbb7ce944df794a8003b58d1d8d94b61c7c
Faris Naji <fnaji@pamgene.com>
2019-02-12 14:33:44 +0100

    added packrat and changed readme

--- README.md
+++ README.md
@@ -5,37 +5,41 @@
 
 ##### Usage
 
-__How to run__
+The file converter requires R to run, R comes with Tercen Desktop
+Install Tercen Desktop, this will also install R. If you have already an R installation then proceed to the R section
 
-Install Tercen Desktop, this will also install R
+Install Tercen Desktop
+Download or clone the complete repository
 
-`git clone https://github.com/tercen/fcs2csv_fileconverter`
+```
+git clone https://github.com/tercen/fcs2csv_fileconverter`
+```
 
-`cd fcs2csv_fileconverter`
+To run on Windows:
+Go to the downloaded folder and double click on the folder
 
-Run on Windows:
+```
+run-win.bat
+```
 
-double click `run-win.bat` 
+* A dialog appears for user to choose a csv file to convert
+* Hit any key to close the black command DOS shell
 
-A dialog appears for user to choose a csv file to convert
+Takes a fcs file as input
 
-Hit any key to close the black command dos shell
 
-Run on linux:
+##### Usage with a previous R installation
 
-make a shell script
+First locate where the R installation directory is located.
 
-__Input file__
-
-Takes a cvs fcs file as input
-
-__Parameters__
+Modify the `run-win.bat`  file and change the location indicated by the `RSCRIPT_EXE` 
 
 `RSCRIPT_EXE` represents the location of the RScript.exe, default is the RScript.exe which comes with Tercen Desktop.
 
-__Output file__
+##### Output file
 
-Same name as the input file but with a fcs id and a timestamp
+Same name as the input file but with a suffix `-tercen`
+It contains an extra column with `file_name`
 
 
 ##### Details
--- fileconvert.R
+++ fileconvert.R
@@ -1,4 +1,11 @@
 #select file
+
+source("packrat/init.R")
+
+# print(file.exists("packrat/init.R"))
+# print(getwd())
+# print(.libPaths())
+
 library(flowCore)
 
 in_file_name <- file.choose()
@@ -13,7 +20,7 @@ get_name_parts <- function(a_file_name) {
   path_sans_base_and_ext <-substr(a_file_name, start=1, stop = (nchar(a_file_name)-nchar(basename))) 
   basename_sans_ext <- substr(start = 1, stop = (nchar(basename)-nchar(ext)-1), basename)
   path_without_ext <- paste0(path_sans_base_and_ext, basename_sans_ext)
-  return(list(basename = basename, path_sans_base_and_ext=path_sans_base_and_ext, path_without_ext = path_without_ext, ext = ext, prefix = prefix))
+  return(list(basename = basename, basename_sans_ext = basename_sans_ext,  path_sans_base_and_ext=path_sans_base_and_ext, path_without_ext = path_without_ext, ext = ext, prefix = prefix))
 }
 
 # filename
@@ -26,12 +33,33 @@ print("reading started...")
 print("...it may take some time..")
 print("...please wait...")
 
-data <- read.FCS(filename = in_file_name, transformation = FALSE)
+# read data in
+data_fcs <- read.FCS(filename = in_file_name, transformation = FALSE)
 
-data <- exprs(data)
+# get parameter names
+names_parameters <- data_fcs@parameters@data$desc
+
+# get data
+data <- exprs(data_fcs)
+
+# get data columns
+col_names <- colnames(data)
+
+# rename parameters
+names_parameters <- ifelse(is.na(names_parameters),col_names,names_parameters)
+colnames(data)   <- names_parameters
   
-file_name <- paste0(file_part$path_without_ext,"-", format(Sys.time(), "%y%m%d%H%M"), ".csv")
 
-write.table(x= data, file = file_name, sep=",", na ="", row.names = FALSE)
+# file_name <- paste0(file_part$path_without_ext,"-", format(Sys.time(), "%y%m%d%H%M"), ".csv")
+
+filename_path <- paste0(file_part$path_without_ext,"-tercen",".csv")
+if (file.exists(filename_path)) file.remove(filename_path)
+
+file_name <- get_name_parts(filename_path)$basename_sans_ext
+
+# add filename to data
+data <- cbind(data, file_name)
+
+write.table(x= data, file = filename_path, sep=",", na ="", row.names = FALSE)
 
 print ("conversion finished")
